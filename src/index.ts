b707807007f7c23768bcedf33e8c1e05a88b7fb3
g0ren <g0ren@proton.me>
2024-07-10 01:48:59 +0200

    add basic express code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import express, {type Express, type Request, type Response } from 'express';
+
+// Create Express instance
+const app: Express = express();
+
+// Disable framework broadcasting
+app.disable('x-powered-by');
+
+// Mock serving the main page
+app.get('/', (req, res) => {
+    res.send('This is the main endpoint response');
+})
+
+// Process any other endpoints that were not considered
+app.use((req: Request, res: Response) => {
+    res.status(404).send('This endpoint is not found');
+})
+
+// Start listening
+app.listen(3000, () => {
+    console.log(`Server is up and running on port 3000`);
+});
+
+
