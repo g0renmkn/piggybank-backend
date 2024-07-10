a0c4b3666cf1f854de20bb6fb62af4eea9b70d9d
g0ren <g0ren@proton.me>
2024-07-10 02:41:04 +0200

    add basic configuration module

---
+++ src/cfg.ts
@@ -0,0 +1,28 @@
+import {config} from "dotenv";
+
+// Check the environment we are running (test, dev, or other)
+if (process.env.BUN_ENV=="test" || process.env.BUN_ENV=="dev") {
+    console.log(`[CONFIG] Loading '.env.${process.env.BUN_ENV}'...`);
+    config({
+        path: `./.env.${process.env.BUN_ENV}`, 
+        override: true
+    });
+}
+else {
+    console.log("[CONFIG] Loading default env file");
+    config();
+}
+
+/* Load default values in case they were not specified */
+// config();
+
+export const cfg = {
+    server_port: process.env.SRV_PORT || 4343,
+    db_host: process.env.DB_HOST || "localhost",
+    db_port: process.env.DB_PORT || 3306,
+    db_user: process.env.DB_USER || "user",
+    db_pass: process.env.DB_PASS || "pass",
+    db_name: process.env.DB_NAME || "database"
+};
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {type Express, type Request, type Response } from 'express';
+import {cfg} from './cfg.ts';
 
 // Create Express instance
 const app: Express = express();
@@ -6,19 +7,22 @@ const app: Express = express();
 // Disable framework broadcasting
 app.disable('x-powered-by');
 
+// Process all the requests as json
+app.use(express.json());
+
 // Mock serving the main page
 app.get('/', (req, res) => {
-    res.send('This is the main endpoint response');
+    res.json({message: 'This is the main endpoint response'});
 })
 
 // Process any other endpoints that were not considered
 app.use((req: Request, res: Response) => {
-    res.status(404).send('This endpoint is not found');
+    res.status(404).json({message: 'This endpoint is not found'});
 })
 
 // Start listening
-app.listen(3000, () => {
-    console.log(`Server is up and running on port 3000`);
+app.listen(cfg.server_port, () => {
+    console.log(`Server is up and running on port ${cfg.server_port}`);
 });
 
 
