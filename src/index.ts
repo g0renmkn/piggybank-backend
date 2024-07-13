d66ca5b92295085ae6aaae25c3cad8703acda76c
g0ren <g0ren@proton.me>
2024-07-13 04:44:19 +0200

    remove mock endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,13 @@ const app: Express = express();
 app.disable('x-powered-by');    // Disable framework broadcasting
 app.use(express.json());        // Process all the requests as json
 
-// Mock serving the main page
-app.get('/', (req: Request, res: Response) => {
-    res.json({message: 'This is the main endpoint response'});
-})
-
 // Include routes
 app.use('', infoRoute);             // /info endpoint route
 app.use('/static', staticRoutes);   // /static routes
 
 // Process any other endpoints that were not considered
 app.use((req: Request, res: Response) => {
-    res.status(404).json({message: 'This endpoint is not found'});
+    res.status(404).json({message: 'Cannot find requested endpoint'});
 })
 
 // Start listening
