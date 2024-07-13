d477079e77969aa7bb1f0f71a1a21e5df9a7547a
g0ren <g0ren@proton.me>
2024-07-13 04:02:08 +0200

    [issue #2] refactor to use a controller and a model for the static tables

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -1,54 +1,16 @@
 /**
  * static.ts
  * 
- * Implementation of the /static route for static table endpoints. No controller needed since this is a very basic endpoint.
+ * Implementation of the /static route for static table endpoints.
  * 
  */
 import { Router, type Request, type Response } from "express";
+import StaticController from '../controllers/static.ts';
 
 const routes: Router = Router();
 
-routes.get("/movtypes", (req: Request, res: Response) => {
-    const movtypes = [
-        "deposit",
-        "withdrawal",
-        "trade",
-        "airdrop",
-        "mining_reward",
-        "fut_open_long",
-        "fut_close_long",
-        "fut_liq_long",
-        "fut_open_short",
-        "fut_close_short",
-        "fut_liq_short"
-    ];
-
-    res.status(200).json(movtypes);
-});
-
-routes.get("/bankperiodicities", (req: Request, res: Response) => {
-    const periodicities = [
-        "one_time",
-        "weekly",
-        "biweekly",
-        "monthly",
-        "bimonthly",
-        "quarterly",
-        "yearly"
-    ];
-
-    res.status(200).json(periodicities);
-});
-
-routes.get("/assettypes", (req: Request, res: Response) => {
-    const assettypes = [
-        "fiat",
-        "crypto",
-        "stock",
-        "fund"
-    ];
-
-    res.status(200).json(assettypes);
-});
+routes.get("/movtypes", StaticController.getMovTypes);
+routes.get("/bankperiodicities", StaticController.getBankPeriodicities);
+routes.get("/assettypes", StaticController.getAssetTypes);
 
 export default routes;
