795e8f67320ee3d6e252bf72a596fd2f303c952b
g0ren <g0ren@proton.me>
2024-09-11 16:48:46 +0200

    [issue #16] allow model selection in the config for the main app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import PiggyApp from './app.ts';
 import { PiggybankModelVar } from './models/PiggybankModelVar.ts';
+import { PiggybankModelMysql } from './models/PiggybankModelMysql.ts';
+import { cfg } from './cfg.ts';
+import { type PiggybankModel } from './models/ModelDefinitions.ts';
 
-const app = new PiggyApp(new PiggybankModelVar())
+let model: PiggybankModel;
+
+// Choose data model
+if (cfg.model==='mysql') {
+    model = new PiggybankModelMysql();
+}
+else {
+    model = new PiggybankModelVar();
+}
+
+const app = new PiggyApp(model);
 
 app.listen();
