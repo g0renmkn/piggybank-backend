a0d643cd06a5f6bf2e1a21124cf491aa48b2496c
g0ren <g0ren@proton.me>
2024-10-16 00:55:51 +0200

    [issue #19] implement tests for new model methods related to categories

--- src/tests/auto/unit_PiggybankModel.test.ts
+++ src/tests/auto/unit_PiggybankModel.test.ts
@@ -1,7 +1,10 @@
 import { describe, expect, it } from "bun:test";
 import { PiggybankModelVar } from '../../models/PiggybankModelVar.ts';
 import { PiggybankModelMysql } from "../../models/PiggybankModelMysql.ts";
-import { generateValidBankAccount } from "./utils.ts";
+import { 
+    generateValidBankAccount,
+    generateValidBankCategory
+} from "./utils.ts";
 import { PBDuplicateRecord, PBNotFoundError } from "../../models/PiggybankModelErrors.ts";
 import { cfg } from "../../cfg.ts";
 
@@ -267,6 +270,256 @@ describe.each([  // run tests for each model implementation
 
             await model.deleteAllBankAccounts();
             expect(await model.getBankAccounts()).toBeEmpty();
+        });
+    });
+
+    // TEST SUITE - create bank category
+    describe('createBankCategory()', () => {
+        // TEST - failure due to duplicated record
+        it('Should fail when adding an already existing record', async () => {
+            let errorRaised = false;
+
+            const wrappedFunction = async () =>  {
+                const categoryRecord = generateValidBankCategory();
+                const model = new modelImplementation(modelOpts);
+                
+                await model.initModel();
+    
+                await model.createBankCategory([categoryRecord]);
+                await model.createBankCategory([categoryRecord]);
+            }
+
+            try {
+                await wrappedFunction();
+            }
+            catch(err: any) {
+                expect(err).toBeInstanceOf(PBDuplicateRecord);
+                errorRaised = true;
+            }
+
+            expect(errorRaised).toBe(true);
+        });
+
+        // TEST - failure due to duplicated record in the same attempt
+        it('Should fail when adding a duplicated record in the same attempt', async () => {
+            let errorRaised = false;
+
+            const wrappedFunction = async () =>  {
+                const categoryRecord = generateValidBankCategory();
+                const model = new modelImplementation(modelOpts);
+                await model.initModel();
+    
+                await model.createBankCategory([categoryRecord, categoryRecord]);
+            }
+
+            try {
+                await wrappedFunction();
+            }
+            catch(err: any) {
+                expect(err).toBeInstanceOf(PBDuplicateRecord);
+                errorRaised = true;
+            }
+
+            expect(errorRaised).toBe(true);
+        });
+
+        // TEST - successful creation of categories
+        it('Should successfully add the generated categories', async () => {
+            const rndNumber = Math.floor(10*Math.random() + 1);
+            const newRecords = []
+            const model = new modelImplementation(modelOpts);
+            
+            await model.initModel();
+
+            // Create a bunch of bank categories
+            for(let i=0; i<rndNumber; i++) {
+                newRecords.push(generateValidBankCategory());
+            }
+            const createdRecords = await model.createBankCategory(newRecords);
+
+            expect(createdRecords.length).toBe(rndNumber);
+            // Check for each added record, that it was properly created
+            newRecords.forEach((rec) => {
+                expect(createdRecords).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            name: rec.name,
+                            description: rec.description,
+                            icon: rec.icon
+                        })
+                    ])
+                );
+            });
+        }); 
+    });
+
+    // TEST SUITE - get bank categories list
+    describe('getBankCategories()', () => {
+        // TEST - get all records (empty data)
+        it('Should return an empty array when no records are yet added', async () => {
+            const model = new modelImplementation(modelOpts);
+            await model.initModel();
+            
+            await model.deleteAllBankCategories();
+            const ret = await model.getBankCategories();
+
+            expect(ret).toBeArrayOfSize(0);
+        });
+        
+        // TEST - get all records (non empty)
+        it('Should return an array with the correctly added records', async () => {
+            const rndNumber = Math.floor(10*Math.random() + 1);
+            const records = []
+            const model = new modelImplementation(modelOpts);
+
+            await model.initModel();
+
+            // Create a bunch of bank categories
+            for(let i=0; i<rndNumber; i++) {
+                records.push(generateValidBankCategory());
+            }
+            await model.createBankCategory(records);
+
+            // Check the generated categories
+            const ret = await model.getBankCategories();
+            expect(ret).toBeArrayOfSize(rndNumber);
+            
+            // Check each record
+            records.forEach((rec) => {
+                expect(ret).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            name: rec.name,
+                            description: rec.description,
+                            icon: rec.icon
+                        })
+                    ])
+                );
+            });
+        });
+    });
+
+    // TEST SUITE - update bank category
+    describe('updateBankCategory()', () => {
+        // TEST - failure due to incorrect ID
+        it('Should throw an error when trying to update a non existing record', async () => {
+            let errorRaised = false;
+
+            const wrappedFunction = async () =>  {
+                const categoryRecord = generateValidBankCategory();
+                const model = new modelImplementation(modelOpts);
+                const modification = {
+                    name: "random name"
+                }
+
+                await model.initModel();
+    
+                // First create an category
+                await model.createBankCategory([categoryRecord]);
+
+                // Then update an category with wrong ID
+                await model.updateBankCategory(345, modification);
+            };
+
+            try {
+                await wrappedFunction();
+            }
+            catch(err: any) {
+                expect(err).toBeInstanceOf(PBNotFoundError);
+                errorRaised = true;
+            }
+
+            expect(errorRaised).toBe(true);
+        });
+        
+        // TEST - successful update of a bank category
+        it('Should correctly update the selected record', async () => {
+            const categoryRecord = generateValidBankCategory();
+            const model = new modelImplementation(modelOpts);
+            const modification = {
+                name: "random name"
+            };
+
+            await model.initModel();
+
+            // First create an category
+            const {id, ...tmp} = (await model.createBankCategory([categoryRecord]))[0];
+
+            // Then update an category with wrong ID
+            const modifiedRecord = await model.updateBankCategory(id, modification);
+
+            // Check against the modified object
+            expect(modifiedRecord).toMatchObject({id, ...categoryRecord, ...modification});
+        });
+    });
+
+    // TEST SUITE - delete bank category
+    describe('deleteBankCategory()', () => {
+        // TEST - failure due to wrong ID
+        it('Should fail due to wrongly provided ID', async () => {
+            let errorRaised = false;
+
+            const wrappedFunction = async () =>  {
+                const categoryRecord = generateValidBankCategory();
+                const model = new modelImplementation(modelOpts);
+
+                await model.initModel();
+    
+                // First create an category
+                await model.createBankCategory([categoryRecord]);
+
+                // Then delete an category with wrong ID
+                await model.deleteBankCategory(345);
+            };
+
+            try {
+                await wrappedFunction();
+            }
+            catch(err: any) {
+                expect(err).toBeInstanceOf(PBNotFoundError);
+                errorRaised = true;
+            }
+
+            expect(errorRaised).toBe(true);
+        })
+
+        // TEST - successfully delete a bank category
+        it('Should succeed correctly deleting the specified category', async () => {
+            const categoryRecord = generateValidBankCategory();
+            const model = new modelImplementation(modelOpts);
+
+            await model.initModel();
+            await model.deleteAllBankCategories();
+
+            // First create an category
+            const [{id, ...tmp}] = await model.createBankCategory([categoryRecord]);
+
+            // Then delete an category with wrong ID
+            const deletedRecord = await model.deleteBankCategory(id);
+
+            expect(deletedRecord).toMatchObject({id, ...categoryRecord});
+            expect(await model.getBankCategories()).toBeEmpty();
+        })
+    });
+
+    // TEST SUITE - delete all bank categories
+    describe('deleteAllBankCategories()', () => {
+        // TEST - delete all categories
+        it('Should successfully delete all categories', async () => {
+            const records = [
+                generateValidBankCategory(),
+                generateValidBankCategory(),
+                generateValidBankCategory()
+            ];
+            const model = new modelImplementation(modelOpts);
+
+            await model.initModel();
+
+            await model.createBankCategory(records);
+            expect(await model.getBankCategories()).not.toBeEmpty();
+
+            await model.deleteAllBankCategories();
+            expect(await model.getBankCategories()).toBeEmpty();
         });
     });
 });
--- src/tests/auto/utils.ts
+++ src/tests/auto/utils.ts
@@ -10,3 +10,12 @@ export function generateValidBankAccount() {
         pfp: faker.string.sample({min: 5, max: 20})
     }
 }
+
+// Generate a valid category object
+export function generateValidBankCategory() {
+    return {
+        name: faker.string.sample({min: 30, max: 30}),
+        description: faker.string.sample({min: 200, max: 200}),
+        icon: faker.string.sample({min: 5, max: 50}),
+    }
+}
